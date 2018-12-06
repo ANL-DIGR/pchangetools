2a6b44bc2867ec9ae7c7ebdae1adf2329ade6e3a
Scott Collis <scollis.acrf@gmail.com>
2018-12-06 14:23:02 -0600

    FIX: pep8

--- pchangetools/__init__.py
+++ pchangetools/__init__.py
@@ -2,10 +2,11 @@
 pchangetools: Tools for the Precipitating change NSF project
 ============================================================
 """
+from . import analysis_tools
 
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-from . import analysis_tools
+__all__ = [s for s in dir() if not s.startswith('_')]
